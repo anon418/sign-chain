58d6e44bc6d506acf2100c3865653f384e82c407
anon418 <anondz619219@gmail.com>
2025-06-16 22:21:02 +0900

    Update genSecrets.js

--- genSecrets.js
+++ genSecrets.js
@@ -4,32 +4,32 @@ const crypto = require('crypto')
 function genLocal() {
   const key = crypto.randomBytes(32).toString('hex')
   const salt = crypto.randomBytes(16).toString('hex')
-  // console.log('LOCAL_KEY=' + key)
-  // console.log('LOCAL_SALT=' + salt)
+  console.log('LOCAL_KEY=' + key)
+  console.log('LOCAL_SALT=' + salt)
 }
 
 function genAes() {
   const aesKey = crypto.randomBytes(32).toString('hex')
-  // console.log('AES_KEY=' + aesKey)
+  console.log('AES_KEY=' + aesKey)
 }
 
 function genJwt() {
   const jwtSecret = crypto.randomBytes(32).toString('hex')
-  // console.log('JWT_SECRET=' + jwtSecret)
+  console.log('JWT_SECRET=' + jwtSecret)
 }
 
 function genRefreshJwt() {
   const refreshJwtSecret = crypto.randomBytes(32).toString('hex')
-  // console.log('REFRESH_JWT_SECRET=' + refreshJwtSecret)
+  console.log('REFRESH_JWT_SECRET=' + refreshJwtSecret)
 }
 
 function usage() {
-  // console.log('Usage: node genSecrets.js [local|aes|jwt|refresh|all]')
-  // console.log('  local   : LOCAL_KEY, LOCAL_SALT 출력')
-  // console.log('  aes     : AES_KEY(64자리 hex) 출력')
-  // console.log('  jwt     : JWT_SECRET(64자리 hex) 출력')
-  // console.log('  refresh : REFRESH_JWT_SECRET(64자리 hex) 출력')
-  // console.log('  all     : 위 4개 모두 출력')
+  console.log('Usage: node genSecrets.js [local|aes|jwt|refresh|all]')
+  console.log('  local   : LOCAL_KEY, LOCAL_SALT 출력')
+  console.log('  aes     : AES_KEY(64자리 hex) 출력')
+  console.log('  jwt     : JWT_SECRET(64자리 hex) 출력')
+  console.log('  refresh : REFRESH_JWT_SECRET(64자리 hex) 출력')
+  console.log('  all     : 위 4개 모두 출력')
 }
 
 const arg = process.argv[2]
